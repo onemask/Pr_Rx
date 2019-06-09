e3b73a846cf6db966c130c3a7ae882d8e9e860f2
onemask <onemask@naver.com>
2019-06-09 23:31:42 +0900

    first init

---
+++ src/main/java/rx/Subscriber.java
@@ -0,0 +1,17 @@
+package rx;/*
+package another_rx;
+
+public interface Subscriber<T> {
+    //구독 시작 시 처리
+    public void onSubscribe(num1.Subscription subscription);
+
+    //데이터 통지 시 처리
+    public void onNext(T item);
+
+    //에러 통지 시 처리
+    public void onError(Throwable error);
+
+    //완료 통지 시 처리
+    public void onComplete();
+}
+*/
---
+++ src/main/java/rx/processor.java
@@ -0,0 +1,11 @@
+package rx;/*
+package another_rx;
+
+*/
+/**
+ * Publisher와 Subscriber의 기능이 모두 있는 인터페이스
+ *//*
+
+public interface processor<T,R> extends Subscriber<T>,Publisher {
+}
+*/
