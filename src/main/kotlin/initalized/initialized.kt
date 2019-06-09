6b4816bac69a9447409b44d43c0baffe88f7ece4
onemask <onemask@naver.com>
2019-06-09 23:59:44 +0900

    [TODO] What is lazy initialized?

--- src/main/kotlin/initalized/initialized.kt
+++ src/main/kotlin/initalized/initialized.kt
@@ -1,26 +1,35 @@
 package initalized
 
 class initialized {
+}
 
+fun main(){
+    val p = Person("elice")
+    println("1 ${p.emails}")
 }
 
 
-
-class Email(){
+class Email{}
     fun  loadEmail(person : Person) : List<Email>{
         print("${person.name}의 이메일을 가져옴")
-        return person.emails
+        return listOf()
     }
-}
+
 
 
 class Person(val name : String){
     private var _emails : List<Email>? = null
     val emails : List<Email>
     get() {
-        if(emails==null){
-            _emails = Email().loadEmail(this)
+        if(_emails==null){
+            _emails = loadEmail(this)
         }
         return _emails!!
     }
 }
+/*
+
+class Person(val name : String){
+    val email by lazy { Email().loadEmail(this) }
+}
+*/
