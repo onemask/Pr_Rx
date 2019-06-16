999c69c41e41b89042aab7d524d9bd58a790b280
onemask <onemask@naver.com>
2019-06-16 18:53:00 +0900

    Add Reactvice Chapt5

--- src/main/java/chapt5/Schedulers.java
+++ src/main/java/chapt5/Schedulers.java
@@ -1,11 +1,40 @@
 package chapt5;
 
-import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class Schedulers {
     public static void main(String[] args) {
-        Observable.just("What is so difficult?", "to learing rx")
-                .subscribe(System.out::println);
-        
+       /* Observable.just("What is so difficult?", "to learing rx")
+                //.subscribe(System.out::println);
+                .subscribe(System.out.println();;*/
+        //Why is only user in subscirbe in method Reperence?
+
+
+        //결과를 수신할때 처리하는 Observer
+        Observer<String> mObserver = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println("onNext");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+
+
     }
-}
+
+    }
